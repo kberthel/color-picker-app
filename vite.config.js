5dba645e8bd478906f0468d4f1034a8dba46c114
kberthel <225011416+kberthel@users.noreply.github.com>
2025-09-26 02:46:07 +0100

    Fix: Remove merge marker from vite.config.js - Sep 26, 2025

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/color-picker-app/',
-=======
->>>>>>> 4441e2bbc0f3eba80f14eae22d88bdfbfc7c9fd2
   server: {
     open: true
   }
